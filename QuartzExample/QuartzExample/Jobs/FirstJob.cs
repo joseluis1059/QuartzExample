f56a13086ae90e89868686913b3ae698380f5f96
joseluis1059 <joseluis1059@hotmail.com>
2020-08-27 16:31:09 -0600

    Quartz

---
+++ QuartzExample/QuartzExample/Jobs/FirstJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzExample.Jobs
+{
+    public class FirstJob: IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
